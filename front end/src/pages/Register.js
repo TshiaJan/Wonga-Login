3db4f44ef58740ac1faa759c78a06b3b027726af
TshiaJan <123463698+TshiaJan@users.noreply.github.com>
2026-03-05 12:26:30 +0200

    Add registration form component

---
+++ front end/src/pages/Register.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Register() {
+  const [formData, setFormData] = useState({ firstName: '', lastName: '', email: '', password: '' });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:5000/api/auth/register', formData);
+      alert('Registered successfully! Go to login.');
+    } catch (err) {
+      alert('Error registering');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="firstName" placeholder="First Name" onChange={handleChange} required />
+      <input name="lastName" placeholder="Last Name" onChange={handleChange} required />
+      <input name="email" type="email" placeholder="Email" onChange={handleChange} required />
+      <input name="password" type="password" placeholder="Password" onChange={handleChange} required />
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default Register;
